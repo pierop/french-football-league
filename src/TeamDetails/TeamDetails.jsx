864fadd32428c78e5571f54d8e7360bc8e7c1b92
pierop <anais.pieropan@gmail.com>
2019-01-07 20:37:44 +0100

    feature(teamDetails): update TeamDetails component to get team id from query params and display all info related to team

--- src/TeamDetails/TeamDetails.jsx
+++ src/TeamDetails/TeamDetails.jsx
@@ -1,8 +1,128 @@
+import { Button } from '@material-ui/core';
+import { ChevronLeft } from '@material-ui/icons';
 import React, { Component } from 'react';
+import Link from '../wrappers/Link';
+import Logo from '../wrappers/Logo';
+import { Label, SubTitle } from '../wrappers/text';
+import Players from './Players/Players';
+import {
+  GoBackWrapper,
+  GoToWebsite,
+  StadiumContainer,
+  StadiumImage,
+  StadiumInfo,
+  TeamDetailsContainer,
+  TeamHeader,
+  TeamName,
+  TeamWrapper,
+  BlockWithMargin
+} from './style';
 
 class TeamDetails extends Component {
+  state = { team: null };
+
+  componentDidMount() {
+    // Get team id from query params
+    const { id } = this.props.match.params;
+
+    fetch(`https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${id}`)
+      .then(result => result.json())
+      .then(result => {
+        const team = result.teams.map(team => {
+          return {
+            id: team.idTeam,
+            name: team.strTeam,
+            logo: team.strTeamBadge,
+            creationYear: team.intFormedYear,
+            manager: team.strManager,
+            description: team.strDescriptionEN,
+            stadiumName: team.strStadium,
+            stadiumLocation: team.strStadiumLocation,
+            stadiumCapacity: team.intStadiumCapacity,
+            stadiumImage: team.strStadiumThumb,
+            websiteUrl: team.strWebsite
+          };
+        })[0];
+        this.setState({ team });
+      });
+  }
+
   render() {
-    return <div>Team</div>;
+    const { team } = this.state;
+
+    return (
+      <TeamDetailsContainer>
+        <GoBackWrapper>
+          <Link to="/">
+            <Button>
+              <ChevronLeft />
+              Go back to league
+            </Button>
+          </Link>
+        </GoBackWrapper>
+
+        {!!team && (
+          <TeamWrapper>
+            <TeamHeader>
+              <Logo src={team.logo} alt={team.name} />
+              <TeamName>{team.name}</TeamName>
+            </TeamHeader>
+
+            <BlockWithMargin>
+              {!!team.creationYear && (
+                <div>
+                  <Label>Created on</Label>
+                  <span>{team.creationYear}</span>
+                </div>
+              )}
+              {!!team.manager && (
+                <div>
+                  <Label>Manager</Label>
+                  <span>{team.manager}</span>
+                </div>
+              )}
+              {!!team.websiteUrl && (
+                <div>
+                  <Label>Website</Label>
+                  <GoToWebsite
+                    href={`http://${team.websiteUrl}`}
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    {team.websiteUrl}
+                  </GoToWebsite>
+                </div>
+              )}
+            </BlockWithMargin>
+
+            {!!team.description && (
+              <BlockWithMargin as="p">{team.description}</BlockWithMargin>
+            )}
+
+            <SubTitle>{team.stadiumName}</SubTitle>
+            <BlockWithMargin as={StadiumContainer}>
+              {!!team.stadiumImage && <StadiumImage src={team.stadiumImage} />}
+              <StadiumInfo>
+                {!!team.stadiumLocation && (
+                  <div>
+                    <Label>Location</Label>
+                    <span>{team.stadiumLocation}</span>
+                  </div>
+                )}
+                {!!team.stadiumCapacity && (
+                  <div>
+                    <Label>Capacity</Label>
+                    <span>{team.stadiumCapacity} seats</span>
+                  </div>
+                )}
+              </StadiumInfo>
+            </BlockWithMargin>
+
+            <Players teamName={team.name} />
+          </TeamWrapper>
+        )}
+      </TeamDetailsContainer>
+    );
   }
 }
 
---
+++ src/TeamDetails/style.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import theme from '../constants/theme';
+import { Title } from '../wrappers/text';
+import FlexContainer from '../wrappers/FlexContainer';
+
+const TeamDetailsContainer = styled.div`
+  background-color: ${theme.colors.light};
+  min-height: 100vh;
+  height: 100%;
+`;
+
+const GoBackWrapper = styled.div`
+  padding: 1rem;
+`;
+
+const TeamWrapper = styled.div`
+  background-color: white;
+  box-shadow: 0 5px 15px 0 rgba(37, 44, 97, 0.15),
+    0 2px 4px 0 rgba(93, 100, 148, 0.2);
+  margin: 2rem;
+  padding: 2rem;
+`;
+
+const TeamHeader = styled(FlexContainer)`
+  padding: 1rem;
+`;
+
+const TeamName = styled(Title)`
+  flex: 1 1 auto;
+`;
+
+const BlockWithMargin = styled.div`
+  margin: 1rem 0 2rem;
+`;
+
+const GoToWebsite = styled.a`
+  color: ${theme.colors.secondary};
+`;
+
+const StadiumContainer = styled(FlexContainer)`
+  align-items: flex-start;
+`;
+
+const StadiumImage = styled.img`
+  width: 50%;
+
+  @media (${theme.breakpoints.mobile.type}-width:
+    ${theme.breakpoints.mobile.value}px) {
+    width: 100%;
+  }
+`;
+
+const StadiumInfo = styled.div`
+  flex: 1 1 auto;
+`;
+
+export {
+  TeamDetailsContainer,
+  GoBackWrapper,
+  TeamWrapper,
+  TeamHeader,
+  TeamName,
+  BlockWithMargin,
+  GoToWebsite,
+  StadiumContainer,
+  StadiumImage,
+  StadiumInfo
+};
